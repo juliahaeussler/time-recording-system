5db7b4441d7a5d3b8e090c29adf840c475c2ecd5
juliahaeussler <julia@haeuszler.de>
2023-06-26 23:18:25 +0200

    test

--- configs/session.config.js
+++ configs/session.config.js
@@ -1,5 +1,5 @@
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo').default;
 
 module.exports = (app) => {
   app.use(
