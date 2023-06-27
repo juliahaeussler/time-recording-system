a8c36dc378cca22cc6ca4766ccb71cf2804495b4
juliahaeussler <julia@haeuszler.de>
2023-06-27 20:55:44 +0200

    test

--- client/src/helpers/localStorage.js
+++ client/src/helpers/localStorage.js
@@ -2,7 +2,7 @@ export const getFromLocalStorage = (localStorageKey) => {
   let localStorageString = null;
   try {
     localStorageString = window.localStorage.getItem(localStorageKey) || null;
-    // console.log('local', localStorageString)
+    console.log('local', localStorageString)
     return localStorageString;
   } catch (e) {
     // This can actually happen, e.g. on mobile Safari if user has blocked cookies
